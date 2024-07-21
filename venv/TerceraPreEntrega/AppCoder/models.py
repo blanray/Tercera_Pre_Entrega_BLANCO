b0a41bed766cd95fbee186b8cbb851ba74d493ec
blanray <blanray_winner@hotmail.com>
2024-07-21 00:34:26 -0300

    Aplicados cambios en el modelo para agregar ID

--- venv/TerceraPreEntrega/AppCoder/models.py
+++ venv/TerceraPreEntrega/AppCoder/models.py
@@ -3,33 +3,37 @@ from django.db import models
 # Create your models here.
 
 class Curso(models.Model):
+    id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=40)
     camada = models.IntegerField()
 
     def __str__(self):
-        return f'Curso | Nombre: {self.nombre} - Camada: {self.camada}'
+        return f'Curso | Id: {self.id} - Nombre: {self.nombre} - Camada: {self.camada}'
 
 class Estudiante(models.Model):
+    id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     email = models.EmailField()
 
     def __str__(self):
-        return f'Estudiante | Nombre: {self.nombre} - Apellido: {self.apellido} - Email: {self.email}'
+        return f'Estudiante | Id: {self.id} - Nombre: {self.nombre} - Apellido: {self.apellido} - Email: {self.email}'
 
 class Profesor(models.Model):
+    id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     email = models.EmailField()
     profesion = models.CharField(max_length=30)
 
     def __str__(self):
-        return f'Profesor | Nombre: {self.nombre} - Apellido: {self.apellido} - Email: {self.email} - Profesion: {self.profesion}'
+        return f'Profesor | Id: {self.id} - Nombre: {self.nombre} - Apellido: {self.apellido} - Email: {self.email} - Profesion: {self.profesion}'
 
 class Entregable(models.Model):
+    id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=30)
     fechaDeEntrega = models.DateField()
     entregado = models.BooleanField()
 
     def __str__(self):
-        return f'Entrergable | Nombre: {self.nombre} - Fecha de Entrega: {self.fechaDeEntrega} - Entregado: {self.entregado}'
+        return f'Entrergable | Id: {self.id} - Nombre: {self.nombre} - Fecha de Entrega: {self.fechaDeEntrega} - Entregado: {self.entregado}'
